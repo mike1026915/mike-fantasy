a149c68918f11fc5a26be9fff3a20f7506ed5ab3
mike_fang <mike1026915.cs96@g2.nctu.edu.tw>
2021-02-12 20:53:13 +0800

    siteMetadata file

---
+++ src/hooks/useSiteMetadata.js
@@ -0,0 +1,14 @@
+import { useStaticQuery, graphql } from 'gatsby';
+
+/*const useSiteMetadata = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query SiteMetaData {
+
+    `
+  );
+
+  return site.siteMetadata;
+};*/
+
+export default useSiteMetadata;
